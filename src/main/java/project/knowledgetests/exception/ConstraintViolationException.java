1948e776310f700aae01b1e8a4c7a7d5fc0e9d80
Andre Martins <fmartins.andre@gmail.com>
2021-11-03 17:58:23 -0300

    Rename ResourceAlreadyExistsException to ConstraintViolationException

--- src/main/java/project/knowledgetests/exception/ConstraintViolationException.java
+++ src/main/java/project/knowledgetests/exception/ConstraintViolationException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
-public class ResourceAlreadyExistsException extends RuntimeException {
+public class ConstraintViolationException extends RuntimeException {
 
-    public ResourceAlreadyExistsException(String message) {
+    public ConstraintViolationException(String message) {
         super(message);
     }
 }
