90e47c25ea6cc841a1e2defa2423019bce2a4949
Andre Martins <fmartins.andre@gmail.com>
2021-11-04 15:04:05 -0300

    Fix validation regex patter

--- src/main/java/project/knowledgetests/contract/user/UserRequestDTO.java
+++ src/main/java/project/knowledgetests/contract/user/UserRequestDTO.java
@@ -5,9 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 @Data
 @Builder
@@ -17,9 +15,11 @@ public class UserRequestDTO {
 
     private Long id;
 
-    @Pattern(regexp = "^[\\p{Alnum}]{3,50}$", message = "The username field must have between 3 and 50 characters long.")
+    @Pattern(regexp = "^[\\w][\\w\\\\.-]{2,49}$",
+            message = "The username field must have between 3 and 50 alphanumeric characters long.")
     private String username;
 
-    @Pattern(regexp = "(^$|^[\\p{Alnum}]{5,150}$)", message = "The fullName field must have between 5 and 150 characters long.")
+    @Pattern(regexp = "(^$|^[\\p{L}\\p{N}\\s_.-]{5,150}$)",
+            message = "The fullName field must have between 5 and 150 characters long.")
     private String fullName;
 }
