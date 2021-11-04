b4c41525492291db1c2e799915aaeb708ebdc2cb
Andre Martins <fmartins.andre@gmail.com>
2021-11-04 08:30:04 -0300

    Add user reference
    Add user reference to answer and make it a unique constraint together with questionReference

--- src/main/java/project/knowledgetests/entity/UserAnswer.java
+++ src/main/java/project/knowledgetests/entity/UserAnswer.java
@@ -15,7 +15,7 @@ import java.time.OffsetDateTime;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(uniqueConstraints = { @UniqueConstraint( columnNames = {"QUESTION_REFERENCE_ID", "QUESTION"} )})
+@Table(uniqueConstraints = { @UniqueConstraint( columnNames = {"QUESTION_REFERENCE_ID", "USER_ID"} )})
 public class UserAnswer {
 
     @Id
@@ -25,6 +25,10 @@ public class UserAnswer {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(nullable = false, updatable = false, name = "QUESTION_REFERENCE_ID")
     private Question questionReference;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, updatable = false, name = "USER_ID")
+    private User user;
 
     @Column(nullable = false, updatable = false, length = 4096)
     private String question;
