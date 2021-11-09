2318eaf08d0fd33921ab6c7b095f78c665454056
Andre Martins <fmartins.andre@gmail.com>
2021-11-09 17:09:16 -0300

    Add a class to make easier to get env variables in code

---
+++ src/main/java/project/knowledgetests/config/EnvironmentConfiguration.java
@@ -0,0 +1,23 @@
+package project.knowledgetests.config;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySource(value = {"classpath:application.properties"})
+public class EnvironmentConfiguration implements EnvironmentAware {
+
+    private static Environment environment;
+
+    public static String get(String key, String defaultValue) {
+        return environment.getProperty(key, defaultValue);
+    }
+
+    @Override
+    public void setEnvironment(@NotNull Environment environment) {
+        EnvironmentConfiguration.environment = environment;
+    }
+}
